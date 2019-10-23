f14359fade1eff9db87b78983afe9bbf00a62567
Adrian Crawford <osspade@users.noreply.github.com>
2019-10-22 22:00:21 -0400

    Nativescript 6 compat

--- patch-platforms.js
+++ patch-platforms.js
@@ -1,6 +1,8 @@
-module.exports = function ($logger, $projectData, $platformsData, hookArgs) {
+module.exports = function($logger, $projectData, $injector, hookArgs) {
     var path = require("path");
-    var platformName = hookArgs.platform.toLowerCase();
+
+    const $platformsData = getPlatformsData($injector);
+    const platformName = (hookArgs.checkForChangesOpts && hookArgs.checkForChangesOpts.platform) || (hookArgs.prepareData && hookArgs.prepareData.platform);
 
     //appDestinationDir is the platform specific app folder. For Android, this will be YOUR_PROJECT_ROOT/platforms/android/src/main/assets/
     var appDestinationDir = $platformsData.getPlatformData(platformName).appDestinationDirectoryPath;
@@ -11,4 +13,13 @@ module.exports = function ($logger, $projectData, $platformsData, hookArgs) {
     var patchNpmPackage = require(patchNpmPackageDir);
 
     patchNpmPackage($logger, $projectData, hookArgs.changesInfo);
+}
+
+
+function getPlatformsData($injector) {
+    try {
+        return $injector.resolve("platformsData");
+    } catch (err) {
+        return $injector.resolve("platformsDataService");
+    }
 }
