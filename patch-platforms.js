9263d4eca9e26c87e408e59c78b6f09a01363f3e
Adrian Crawford <osspade@users.noreply.github.com>
2019-10-22 22:35:57 -0400

    Update patch-platforms.js

--- patch-platforms.js
+++ patch-platforms.js
@@ -3,14 +3,7 @@ module.exports = function($logger, $projectData, $injector, hookArgs) {
 
     const $platformsData = getPlatformsData($injector);
     const platformName = (hookArgs.checkForChangesOpts && hookArgs.checkForChangesOpts.platform) || (hookArgs.prepareData && hookArgs.prepareData.platform);
-
-    //appDestinationDir is the platform specific app folder. For Android, this will be YOUR_PROJECT_ROOT/platforms/android/src/main/assets/
-    var appDestinationDir = $platformsData.getPlatformData(platformName).appDestinationDirectoryPath;
-    var patchNpmPackageDir = path.join(appDestinationDir, "app", "tns_modules", "nativescript-nodeify", "patch-npm-packages.js");
-
-    //Calling patch-npm-packages.js under platforms to patch the node_modules in the platforms folder.
-    //This will leave the source code folder YOUR_PROJECT_ROOT/node_modules untouched.
-    var patchNpmPackage = require(patchNpmPackageDir);
+    const patchNpmPackage = require("./patch-npm-packages.js");
 
     patchNpmPackage($logger, $projectData, hookArgs.changesInfo);
 }
